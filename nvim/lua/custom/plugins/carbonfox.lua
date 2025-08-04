f2a1ec2d1e9fcd6bce7e9c2c3a823625eda6e791
MichaelSandilands <michael.b.sandilands@gmail.com>
2025-08-04 11:22:10 +0100

    Added neovim config and placeholder for tmux

---
+++ nvim/lua/custom/plugins/carbonfox.lua
@@ -0,0 +1,9 @@
+return {
+  'EdenEast/nightfox.nvim',
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require('nightfox').setup {}
+    -- Enable theme
+    vim.cmd.colorscheme 'carbonfox'
+  end,
+}
