e9f5671b7101969fddf7cd2570644a57f30b9927
MichaelSandilands <michael.b.sandilands@gmail.com>
2025-08-08 18:49:33 +0100

    expanded mini.nvim

--- nvim/.config/nvim/lua/mini/init.lua
+++ nvim/.config/nvim/lua/mini/init.lua
@@ -1,4 +1,46 @@
 -- :help mini.basics
+-- :help mini.ai
+require('mini.ai').setup()
+
+-- use `V` to select lines and `gA` to initiate alignment.
+-- =<CR> is a very helpful command to align with '=' sign
+require('mini.align').setup()
+
+require('mini.comment').setup()
+
+require('mini.completion').setup()
+
+require('mini.keymap').setup()
+
+-- <M-k> move line up <M-j> move line down
+-- works in visual mode too
+require('mini.move').setup()
+
+require('mini.operators').setup()
+
+require('mini.pairs').setup()
+
+local gen_loader = require('mini.snippets').gen_loader
+require('mini.snippets').setup({
+  snippets = {
+    -- Load custom file with global snippets first (adjust for Windows)
+    gen_loader.from_file('~/.config/nvim/snippets/global.json'),
+
+    -- Load snippets based on current language by reading files from
+    -- "snippets/" subdirectories from 'runtimepath' directories.
+    gen_loader.from_lang(),
+  },
+})
+
+
+-- gS break arguments into new lines or join to single line
+require('mini.splitjoin').setup()
+
+require('mini.surround').setup()
+
+-- sa -- Add surrounding in Normal and Visual modes
+-- sd -- Delete surrounding
+-- sr -- Replace surrounding
 require('mini.basics').setup({
 	options = {
 		extra_ui = true,
@@ -9,23 +51,52 @@ require('mini.basics').setup({
 	},
 })
 
--- :help mini.ai
-require('mini.ai').setup()
 
--- use `V` to select lines and `gA` to initiate alignment.
--- =<CR> is a very helpful command to align with '=' sign
-require('mini.align').setup()
+local miniclue = require('mini.clue')
+miniclue.setup({
+  triggers = {
+    -- Leader triggers
+    { mode = 'n', keys = '<Leader>' },
+    { mode = 'x', keys = '<Leader>' },
 
--- <M-k> move line up <M-j> move line down
--- works in visual mode too
-require('mini.move').setup()
+    -- Built-in completion
+    { mode = 'i', keys = '<C-x>' },
 
-require('mini.pairs').setup()
+    -- `g` key
+    { mode = 'n', keys = 'g' },
+    { mode = 'x', keys = 'g' },
 
--- gS break arguments into new lines or join to single line
-require('mini-splitjoin').setup()
+    -- Marks
+    { mode = 'n', keys = "'" },
+    { mode = 'n', keys = '`' },
+    { mode = 'x', keys = "'" },
+    { mode = 'x', keys = '`' },
 
--- sa -- Add surrounding in Normal and Visual modes
--- sd -- Delete surrounding
--- sr -- Replace surrounding
-require('mini-surround').setup()
+    -- Registers
+    { mode = 'n', keys = '"' },
+    { mode = 'x', keys = '"' },
+    { mode = 'i', keys = '<C-r>' },
+    { mode = 'c', keys = '<C-r>' },
+
+    -- Window commands
+    { mode = 'n', keys = '<C-w>' },
+
+    -- `z` key
+    { mode = 'n', keys = 'z' },
+    { mode = 'x', keys = 'z' },
+  },
+
+  clues = {
+    -- Enhance this by adding descriptions for <Leader> mapping groups
+    miniclue.gen_clues.builtin_completion(),
+    miniclue.gen_clues.g(),
+    miniclue.gen_clues.marks(),
+    miniclue.gen_clues.registers(),
+    miniclue.gen_clues.windows(),
+    miniclue.gen_clues.z(),
+  },
+
+  window = {
+	  config = { width = 60},
+  }
+})
