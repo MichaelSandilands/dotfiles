c2ad36a5ff684ecffec2700a4e48a5d06eb14409
MichaelSandilands <michael.b.sandilands@gmail.com>
2025-11-23 22:41:01 +0000

    Added quarto.nvim

---
+++ nvim/.config/nvim/lua/custom/plugins/quarto.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    'quarto-dev/quarto-nvim',
+    dependencies = {
+      'jmbuhr/otter.nvim',
+    },
+  },
+  { -- directly open ipynb files as quarto docuements
+    -- and convert back behind the scenes
+    'GCBallesteros/jupytext.nvim',
+    opts = {
+      custom_language_formatting = {
+        python = {
+          extension = 'qmd',
+          style = 'quarto',
+          force_ft = 'quarto',
+        },
+        r = {
+          extension = 'qmd',
+          style = 'quarto',
+          force_ft = 'quarto',
+        },
+      },
+    },
+  },
+}
